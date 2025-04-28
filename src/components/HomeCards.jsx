6667d4ef59077bcc5c41486167c26448e6464225
David Murff <dmurffkc@gmail.com>
2025-04-28 06:18:15 -0500

    Initial commit

---
+++ src/components/HomeCards.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Card from "./Card";
+import { Link } from "react-router-dom";
+
+const HomeCards = () => {
+  return (
+    <section className="py-4">
+      <div className="container-xl lg:container m-auto">
+        <Card>
+          <h2 className="text-2xl font-bold">For Developers</h2>
+          <p className="mt-2 mb-4">
+            Browse our React jobs and start your career today
+          </p>
+          <Link
+            to="/jobs"
+            className="inline-block bg-black text-white rounded-lg px-4 py-2 hover:bg-gray-700"
+          >
+            Browse Jobs
+          </Link>
+        </Card>
+        <Card bg="bg-indigo-100">
+          <h2 className="text-2xl font-bold">For Employers</h2>
+          <p className="mt-2 mb-4">
+            List your job to find the perfect developer for the role
+          </p>
+          <Link
+            to="/add-job"
+            className="inline-block bg-indigo-500 text-white rounded-lg px-4 py-2 hover:bg-indigo-600"
+          >
+            Add Job
+          </Link>
+        </Card>
+      </div>
+    </section>
+  );
+};
+
+export default HomeCards;
